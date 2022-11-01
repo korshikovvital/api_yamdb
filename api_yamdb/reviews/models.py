57f44e0fe382445b5b57a348091dc35c58886fe7
korshikovvital <korshikovvitaly@gmail.com>
2022-11-01 22:21:51 +0300

    Модели отзывы и комментарии

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -46,3 +46,56 @@ class Title(models.Model):
 class GenreTitle(models.Model):
     genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True)
     tilte = models.ForeignKey(Title, on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+    SCORE = [
+        (i, i) for i in range(11)
+    ]
+    title_id = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведение'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='author',
+        verbose_name='Автор'
+    )
+    score = models.IntegerField(
+        choices=SCORE,
+        verbose_name='Оценка'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+
+    def __str__(self):
+        return self.text
+
+
+class Comments(models.Model):
+    review_id = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='coments',
+        verbose_name='Отзыв'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='coments',
+        verbose_name='Автор'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+
+    def __str__(self):
+        return self.text
